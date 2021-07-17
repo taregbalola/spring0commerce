599ef30f8bdb3de4ecafff8dc69995faa48da1ad
darkmoon <tareg.balola@gmail.com>
2021-07-17 11:58:01 +0200

    edit backend and frontend

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerRepository.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import com.shopme.common.entity.AuthenticationType;
 import com.shopme.common.entity.Customer;
 
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
@@ -14,7 +15,13 @@ public interface CustomerRepository extends CrudRepository<Customer, Integer> {
 	@Query("SELECT c FROM Customer c WHERE c.verificationCode = ?1")
 	public Customer findByVerificationCode(String code);
 	
-	@Query("UPDATE Customer c SET c.enabled = true WHERE c.id = ?1")
+	@Query("UPDATE Customer c SET c.enabled = true, c.verificationCode = null WHERE c.id = ?1")
 	@Modifying
-	public void enable(Integer id);	
+	public void enable(Integer id);
+	
+	@Query("UPDATE Customer c SET c.authenticationType = ?2 WHERE c.id = ?1")
+	@Modifying
+	public void updateAuthenticationType(Integer customerId, AuthenticationType type);
+	
+	public Customer findByResetPasswordToken(String token);
 }
